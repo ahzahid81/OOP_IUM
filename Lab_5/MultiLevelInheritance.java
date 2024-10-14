563726bbbe0d41c968fd4d53dcab44414fa9a9cd
Abdul Hasib Zahid <abdulhasibzahid@gmail.com>
2024-10-14 08:34:41 +0300

    Experiment 10 Done

---
+++ Lab_5/MultiLevelInheritance.java
@@ -0,0 +1,9 @@
+public class MultiLevelInheritance {
+    public static void main(String[] args) {
+        Puppy myPuppy = new Puppy();
+
+        myPuppy.eat();
+        myPuppy.bark();
+        myPuppy.weep();
+    }
+}
---
+++ Lab_5/Puppy.java
@@ -0,0 +1,7 @@
+public class Puppy extends Dog {
+
+    void weep(){
+        System.out.println("The puppy is weeeping.");
+    }
+    
+}
